8daf7dadb3dc393bd4037f15edfc11bc2850b0a8
Pablo <lisadz2@hotmail.com>
2019-06-13 13:13:41 -0500

    initial commit
    initial commit

---
+++ ApiTheMovieDB/ApiTheMovieDB.h
@@ -0,0 +1,19 @@
+//
+//  ApiTheMovieDB.h
+//  ApiTheMovieDB
+//
+//  Created by Pablo Ramirez on 6/13/19.
+//  Copyright © 2019 Pablo Ramirez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ApiTheMovieDB.
+FOUNDATION_EXPORT double ApiTheMovieDBVersionNumber;
+
+//! Project version string for ApiTheMovieDB.
+FOUNDATION_EXPORT const unsigned char ApiTheMovieDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApiTheMovieDB/PublicHeader.h>
+
+
